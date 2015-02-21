13c65bb5382d1874807a80c9725d55d8571ca29e
mike <mike@m>
2015-02-21 15:14:56 -0800

    adding updates to xlsx converter

--- Python/xlsx-to-csv.py
+++ Python/xlsx-to-csv.py
@@ -1,18 +1,19 @@
 import xlrd
 import csv
 import os
+import sys
 
-def xlsx_to_csv():
-    xlwb = xlrd.open_workbook(sys.argv[1])
-    xlsh = xlwb.sheet_by_name(sys.argv[2])
-    new_csv = open(sys.argv[1], 'wb')
+def xlsx_to_csv(wbook, sheet):
+    xlwb = xlrd.open_workbook(wbook)
+    xlsh = xlwb.sheet_by_name(sheet)
+    new_csv = open(wbook[:-4]+"csv", 'wb')
     writer = csv.writer(new_csv, quoting=csv.QUOTE_ALL)
     for row in xrange(xlsh.nrows):
         writer.writerow(xlsh.row_values(row))
     new_csv.close()
 
 def main():
-    xlsx_to_csv()
+    xlsx_to_csv(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
     main()
