8a84850dee1f8ec352202d67109b22e15e986bd7
mbartoli <bartolimichael@gmail.com>
2015-09-15 21:24:37 -0700

    updating

--- Python/stocksort.py
+++ Python/stocksort.py
@@ -5,12 +5,13 @@ def subMergeLIS(i,j,A):
 	numIncreasing = 1
 
 	n = i
-	while (n <= j):
+	while (n < j):
 		if A[n] <= A[n+1]:
 			numIncreasing += 1
-		if numIncreasing > longestSoFar:
-			longestSoFar = numIncreasing
-			bestStartIndex = newStartIndex
+			if numIncreasing > longestSoFar:
+				longestSoFar = numIncreasing
+				bestStartIndex = newStartIndex
+		else:
 			newStartIndex = n+1
 			numIncreasing = 1
 		n = n+1
@@ -19,7 +20,7 @@ def subMergeLIS(i,j,A):
 
 def main():
 	stocks = [1,2,4,3,5,2,3,4]
-	x, y = 0,6
+	x, y = 3,7
 	print subMergeLIS(x,y,stocks)
 
 if __name__ == "__main__":
