f88bc169affb1dac1d3f9b5bf00fa12df44ab4c2
mbartoli <bartolimichael@gmail.com>
2015-09-15 20:45:14 -0700

    adding algs problem

---
+++ Python/stocksort.py
@@ -0,0 +1,26 @@
+def subMergeLIS(i,j,A):
+	bestStartIndex = i
+	newStartIndex = i
+	longestSoFar = 1
+	numIncreasing = 1
+
+	n = i
+	while (n <= j):
+		if A[n] <= A[n+1]:
+			numIncreasing += 1
+		if numIncreasing > longestSoFar:
+			longestSoFar = numIncreasing
+			bestStartIndex = newStartIndex
+			newStartIndex = n+1
+			numIncreasing = 1
+		n = n+1
+	return bestStartIndex, longestSoFar
+
+
+def main():
+	stocks = [1,2,4,3,5,2,3,4]
+	x, y = 0,6
+	print subMergeLIS(x,y,stocks)
+
+if __name__ == "__main__":
+	main()
